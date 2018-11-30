a0fd310f3fac2dd3e345a1e9358828b848bc8295
Fe De <fede_piazza@hotmail.com>
2018-11-30 14:09:08 -0300

    echo return fixed

--- app.js
+++ app.js
@@ -10,42 +10,9 @@ app.use(bodyParse.urlencoded({extended: true}));
 
 app.get("/", async function(req, res){
     res.send(`Nothing to do Here \nBut i have an Echoe for you:\nQuery: ${JSON.stringify(req.query)}
-    Body: ${JSON.stringify(req.body)});
+    Body: ${JSON.stringify(req.body)}`);
 })
 
 app.use("/sticker", router_sticker);
 
 app.listen(8080);
-
-//Old code, waiting to be removed soon
-/*
-app.get("/login",function(req, res){
-    User.find(function(err, doc){
-        console.log(doc);
-        res.render("login");
-    });
-});
-
-app.post("/users",function(req,res){
-    console.log("contraseña: " + req.body.password);
-    console.log("contraseña: " + req.body.email);
-
-    var user = new User({email: req.body.email, 
-                        password: req.body.password,
-                        username: req.body.username})
-    /*user.save(function(err){
-        res.send("Datos guardados");
-    });*//*
-    user.save().then(
-        function(us){
-            res.send("Guardamos los datos");
-        },
-        function(err){
-            if(err){
-                console.log(String(err));
-                res.send("No pudimos guardar los datos");
-            }
-        }
-    );
-});
-*/
