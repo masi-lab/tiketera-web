62d871af13a507347b9e09dd06dccabfab6b93ae
Fe De <fede_piazza@hotmail.com>
2018-12-06 11:24:46 -0300

    new scheduler sticker_log

--- app/sticker/models/sticker.js
+++ app/sticker/models/sticker.js
@@ -5,11 +5,25 @@ mongoose.connect("mongodb://localhost/stiker_0_2", options={useNewUrlParser: tru
 
 var sticker_schema = new Schema(
     {
-        codigo: {type: String, required: true, maxlength: [50, "muy grande la cadena de texto"]},
-        descripcion: {type: String, required: true, maxlength: [50, "muy grande la cadena de texto"]}
+        codigo: {type: String, required: false, maxlength: [50, "muy grande la cadena de texto"]},
+        descripcion: {type: String, required: false, maxlength: [50, "muy grande la cadena de texto"]}
     }
 );
 
 var Sticker = mongoose.model("Sticker", sticker_schema);
 
 module.exports.Sticker = Sticker;
+
+
+var sticker_schema_log = new Schema(
+    {
+        codigo: {type: String, required: false, maxlength: [50, "muy grande la cadena de texto"]},
+        descripcion: {type: String, required: false, maxlength: [50, "muy grande la cadena de texto"]},
+        fecha: {type: Date, default: Date.now(), required: true},
+        cant_impresas: {type: Number, required: false}
+    }
+);
+
+var Sticker_log = mongoose.model("Sticker_log", sticker_schema_log);
+
+module.exports.Sticker_log = Sticker_log;
